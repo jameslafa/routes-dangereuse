f53d34ca36d77d190f3f8362f9621ab7d1646f82
James Lafa <jaaames.lf@gmail.com>
2013-01-08 21:46:02 +0100

    Add vehicule categorie to accessible attribute of the model

--- app/models/accident.rb
+++ app/models/accident.rb
@@ -1,3 +1,7 @@
 class Accident < ActiveRecord::Base
-  attr_accessible :atmospherique, :codeinsee, :collision, :gravite, :hospitalises, :indemnes, :intersection, :latitude, :leger, :longitude, :lumiere, :numac, :route, :tues, :ville
+  attr_accessible :atmospherique, :codeinsee, :collision, :gravite,
+                  :hospitalises, :indemnes, :intersection, :latitude,
+                  :leger, :longitude, :lumiere, :numac, :route, :tues,
+                  :ville, :vehicule_1, :vehicule_2, :vehicule_3, :vehicule_4,
+                  :vehicule_5, :vehicule_6
 end
