f976e92943b09c1280731fdcf1f6179575cc1a1c
James Lafa <jaaames.lf@gmail.com>
2013-01-10 22:53:39 +0100

    Change boolean to integer on PG

--- db/migrate/20130109223956_change_vehicules_data_type_in_accidents.rb
+++ db/migrate/20130109223956_change_vehicules_data_type_in_accidents.rb
@@ -1,19 +1,14 @@
 class ChangeVehiculesDataTypeInAccidents < ActiveRecord::Migration
   def up
-    change_column :accidents, :vehicule_1, :integer
-    change_column :accidents, :vehicule_2, :integer
-    change_column :accidents, :vehicule_3, :integer
-    change_column :accidents, :vehicule_4, :integer
-    change_column :accidents, :vehicule_5, :integer
-    change_column :accidents, :vehicule_6, :integer
+    alter_column :accidents, :vehicule_1, :integer, { true => 1, "else" => nil }, true
+    alter_column :accidents, :vehicule_2, :integer, { true => 1, "else" => nil }, true
+    alter_column :accidents, :vehicule_3, :integer, { true => 1, "else" => nil }, true
+    alter_column :accidents, :vehicule_4, :integer, { true => 1, "else" => nil }, true
+    alter_column :accidents, :vehicule_5, :integer, { true => 1, "else" => nil }, true
+    alter_column :accidents, :vehicule_6, :integer, { true => 1, "else" => nil }, true
   end
 
   def down
-    change_column :accidents, :vehicule_1, :boolean
-    change_column :accidents, :vehicule_2, :boolean
-    change_column :accidents, :vehicule_3, :boolean
-    change_column :accidents, :vehicule_4, :boolean
-    change_column :accidents, :vehicule_5, :boolean
-    change_column :accidents, :vehicule_6, :boolean
+    raise ActiveRecord::IrreversibleMigration.new
   end
 end
